704fa28ef3b68c2eb137d7de78e5c8a5d6d315b6
S. Kyle Walton <skylewalton@gmail.com>
2019-08-22 11:29:51 -0400

    landing-page-update

--- movie-ui/src/Components/Home.js
+++ movie-ui/src/Components/Home.js
@@ -7,17 +7,42 @@ function home() {
   return (
     <div>
       <Navbar />
-      <div class="jumbotron">
-        <div class="container">
-          <h1 class="display-4">Coming To America</h1>
-          <p class="lead">This week's movie of the week is Coming To America.</p>
-          <p>When you think of garbage, think of Akeem!</p>
-          <br />
-          <p class="lead">
-            <a class="btn btn-primary btn-lg discussionbutton" href="#" role="button">JOIN THE DISCUSSION</a> </p>
+
+      {/*<div className="row jumbo-cont">*/}
+      {/*  <div className="col-10 offset-1">*/}
+      {/*    <div class="jumbotron border-dark mb-3">*/}
+      {/*      <div class="container">*/}
+      {/*        <img class="jumbotron-img" src="https://www.indiewire.com/wp-content/uploads/2012/11/Casablanca-Poster.jpg"/>*/}
+      {/*      </div>*/}
+      {/*        <p className="lead">“It's still the same old story, a fight for love and glory.”</p>*/}
+      {/*          <p class="lead">*/}
+      {/*          <a class="home-btn btn btn-primary btn-lg discussionbutton" href="#" role="button">JOIN THE DISCUSSION</a> </p>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
+
+      <div class="title-row">
+      <div className="row">
+        <div className="col-sm-4 home-card-title">
+          <div className="card border-0">
+            <div className="card-body">
+              <h5 className="card-title">Movie of the Week!</h5>
+              <p className="card-text">"So you see, my son, there is a very fine line between love and nausea."</p>
+              <a href="#" className="btn btn-primary">Join the Discussion</a>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-8">
+          <div className="card home-card-image">
+            <img className="jumbotron-img"
+                 src="http://www.jetmag.com/wp-content/uploads/2017/04/coming-to-america-sequel-1489674155.jpg"/>
+          </div>
         </div>
       </div>
-      <div class="card-deck">
+      </div>
+
+
+      <div class="card-deck justify-content-center align-self-center">
         <div class="card">
           <img class="card-img-top" src="https://www.indiewire.com/wp-content/uploads/2017/02/shutterstock_7898420om.jpg?w=780" alt="Card image cap" />
           <div class="card-body">
--- movie-ui/src/Components/MovieOfTheWeek.js
+++ movie-ui/src/Components/MovieOfTheWeek.js
@@ -5,10 +5,21 @@ import Navbar from './Navbar';
 function MovieOfTheWeek() {
     return (
         <div>
-            <Navbar />
-            <h1>This is the movie of the week page</h1>
+             <div>
+                  <Navbar />
+                 <h1>Movie of the Week!</h1>
+             </div>
+            <row>
+             <div className="card w-70">
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" className="btn btn-primary">Button</a>
+                </div>
+              </div>
+            </row>
         </div>
-    );
-}
+    )
+};
 
 export default MovieOfTheWeek;
