232d4b6983e729d8a4523888f3b5be1090a7b8ef
MauriceHunter <mauriceh1995@yahoo.com>
2019-08-23 13:48:25 -0400

    Work on Comments

--- movie-ui/src/Components/MovieOfTheWeek.js
+++ movie-ui/src/Components/MovieOfTheWeek.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import '../CSS/HomeCSS.css';
 import Navbar from './Navbar';
+import CommentApp from './CommentApp';
 
 function MovieOfTheWeek() {
     return (
         <div>
             <Navbar />
             <h1>This is the movie of the week page</h1>
+            <CommentApp/>
         </div>
     );
 }
