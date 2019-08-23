a6c7896536052db042457bd3d9cd3cb8d394252c
Work Account <ashley.newkirk1@gmail.com>
2019-08-23 14:18:47 -0400

    ashley's commits

---
+++ movie-hub/src/main/java/org/carolinafintechhub/moviehub/repository/CommentRepository.java
@@ -0,0 +1,21 @@
+package org.carolinafintechhub.moviehub.repository;
+
+import org.carolinafintechhub.moviehub.model.Comment;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends CrudRepository<Comment, Long> {
+
+    List<Comment> findAll();
+
+    Comment deleteCommentById(long id);
+
+    Comment save(long id);
+
+    Comment findCommentById(long id);
+
+    Comment editCommentById(long id);
+}
