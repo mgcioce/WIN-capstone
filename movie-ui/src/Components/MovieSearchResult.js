19a5caa202871fbbf9d1cb52f5df8b1b4e2632eb
S. Kyle Walton <skylewalton@gmail.com>
2019-08-28 10:28:23 -0400

    search-result-page

---
+++ movie-ui/src/Components/MovieSearchResult.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../CSS/HomeCSS.css';
+import Navbar from './Navbar.js'
+import Movie from "./Movie";
+
+function MovieSearchResult() {
+    return (
+        <div>
+            <Navbar/>
+
+            <Movie/>
+
+        </div>
+    )
+};
+
+export default MovieSearchResult;
